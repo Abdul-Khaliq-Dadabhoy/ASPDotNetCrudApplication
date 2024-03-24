588e353b2f797aa8b021a9d5875fb6aaeaf9d260
Abdul-Khaliq-Dadabhoy <123827920+Abdul-Khaliq-Dadabhoy@users.noreply.github.com>
2024-03-24 23:14:42 +0500

    Add files via upload

---
+++ Models/MajuContext.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace AbdulKhaliqDotNet.Models;
+
+public partial class MajuContext : DbContext
+{
+    public MajuContext()
+    {
+    }
+
+    public MajuContext(DbContextOptions<MajuContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Student> Students { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+ => optionsBuilder.UseSqlServer("Server=DESKTOP-6A00ROS\\MSSQLSERVERAK;Database=MAJU;Trusted_Connection=True;trustServerCertificate=true");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Student>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Students__3214EC07C7F3B185");
+
+            entity.Property(e => e.Id).ValueGeneratedNever();
+            entity.Property(e => e.Age).HasColumnName("AGE");
+            entity.Property(e => e.Contact)
+                .HasMaxLength(200)
+                .IsUnicode(false)
+                .HasColumnName("CONTACT");
+            entity.Property(e => e.Name)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("NAME");
+            entity.Property(e => e.Semester)
+                .HasMaxLength(200)
+                .IsUnicode(false)
+                .HasColumnName("SEMESTER");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AbdulKhaliqDotNet.Models;
+
+public partial class Student
+{
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? Semester { get; set; }
+
+    public int? Age { get; set; }
+
+    public string? Contact { get; set; }
+}
